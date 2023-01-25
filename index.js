7631f305f4be636d5604dcedfca74eb73c539dad
Isaac <isaac.lin.pnc@gmail.com>
2023-01-25 15:13:26 -0500

    finished lab.

--- index.js
+++ index.js
@@ -20,16 +20,72 @@ function myMap (collection, cb) {
     return newArray
 }
 
-function myReduce(collection, cb, acc = 0) {
-    let array = collection
+function myReduce(collection, cb, acc) {
+    let newCollection = convert(collection)
+    if (!acc) {
+        acc = newCollection[0]
+        newCollection = newCollection.slice(1)
+    }
+    for (let i = 0; i < newCollection.length; i++) {
+        acc = (cb(acc, newCollection[i], newCollection))
+    }
+    return acc
+}
+
+function convert(collection) {
     if (typeof collection === "object") {
-       array = Object.values(collection)
+         collection = Object.values(collection)
+    } else {
+        collection.slice()
     }
-    let start = acc
-    for (let i = 0; i < array.length; i++) {
-        console.log(start)
-        start += (cb(acc = collection, array[i], array))
-        console.log(start)
+    return collection
+}
+
+function myFind(collection, predicate) {
+    let newCollection = convert(collection)
+    for (let i = 0; i < newCollection.length; i++) {
+        if (predicate(newCollection[i])) {
+            return newCollection[i]
+        }
     }
-    return start
+}
+
+function myFilter(collection, predicate) {
+    let newCollection = convert(collection)
+    let newArray = []
+    for (let i = 0; i < newCollection.length; i++) {
+        if (predicate(newCollection[i])) {
+            newArray.push(newCollection[i])
+        }
+    }
+    return newArray
+}
+
+function mySize(collection) {
+    let newCollection = convert(collection)
+    return newCollection.length
+}
+
+function myFirst(array, num) {
+    if (!num) {
+        return array[0]
+    } else {
+    return array.slice(0, num)
+    }
+}
+
+function myLast(array, num) {
+    if (!num) {
+        return array[array.length -1]
+    } else {
+        return array.slice(-num)
+    }
+}
+
+function myKeys(object) {
+    return Object.keys(object)
+}
+
+function myValues(object) {
+    return Object.values(object)
 }
